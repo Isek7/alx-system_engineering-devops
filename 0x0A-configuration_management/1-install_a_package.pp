ea411d64423f5e32e8f988e226606053f3b947dc
Isek7 <bengabrielisek@gmail.com>
2024-01-19 07:53:48 -0800

    Updating

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # install puppet-lint using Puppet
 
-package { 'puppet-lint':
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'gem',
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1', # or the version compatible with Flask 2.1.0
+  provider => 'pip3',
 }
