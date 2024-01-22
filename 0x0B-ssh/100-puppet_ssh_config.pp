fad6029629177b8cc328d5fa64e33b47643b8b18
Isek7 <bengabrielisek@gmail.com>
2024-01-21 16:06:53 -0800

    Using Puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#configuring puppet to make changes to our configuration file
+
+file { 'etc/ssh/ssh_cofig':
+	ensure => present,
+
+content =>"
+
+	#SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+
+}
