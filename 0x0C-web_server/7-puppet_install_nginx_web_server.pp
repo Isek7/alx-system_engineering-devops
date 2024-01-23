39b9b3ca306e38e63a0b13975575ffb50e40e6c3
Isek7 <bengabrielisek@gmail.com>
2024-01-23 13:20:01 -0800

    configuration with nginx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,23 @@
-# automating my work using Puppet
-
+# Installing Nginx package
 package { 'nginx':
   ensure => installed,
 }
 
+# Adding the rewrite line to the Nginx configuration
 file_line { 'install':
-  ensure => 'present',
+  ensure => present,
   path   => '/etc/nginx/sites-enabled/default',
   after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.github.com/Isek7 permanent;',
 }
 
+# Creating the index.html file
 file { '/var/www/html/index.html':
   content => 'Hello World!',
 }
 
+# Ensuring Nginx service is running and requires the Nginx package
 service { 'nginx':
   ensure  => running,
-  require => package['nginx'],
+  require => Package['nginx'],
 }
