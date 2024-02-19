486b12fcbf3b8ab045caa965be43f8c6969c9789
Isek7 <bengabrielisek@gmail.com>
2024-02-19 22:16:40 +0100

    Updated

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,12 +1,49 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+'''
+    for a given employee ID, returns information about
+    his/her TODO list progress.
+'''
 import requests
 import sys
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-    finished = [t.get('title') for t in todos if t.get('completed') is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(finished), len(todos)))
-    [print("\t {}".format(c)) for c in finished]
+# import logging
+
+
+# logging.basicConfig(level=logging.DEBUG)
+def get_username(user_id):
+    '''Gets username of employee with user_id'''
+    user_url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
+    response = requests.get(user_url)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('name')
+
+
+def get_todos(user_id):
+    '''Retrieves todos of employee with user_id'''
+    todo_url = f'https://jsonplaceholder.typicode.com/users/{user_id}/todos'
+    response = requests.get(todo_url)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+
+
+def parse(username, todos):
+    '''Parses data'''
+    completed = []
+    total_tasks = 0
+    for todo in todos:
+        total_tasks += 1
+        if todo.get('completed') is True:
+            completed.append(todo.get('title'))
+    completed_tasks = len(completed)
+    print(f"Employee {username} is done "
+          f"with tasks({completed_tasks}/{total_tasks})")
+    for task in completed:
+        print(f"\t {task}")
+
+
+if __name__ == '__main__':
+    user_id = sys.argv[1]
+    username = get_username(user_id)
+    todos = get_todos(user_id)
+    parse(username, todos)
