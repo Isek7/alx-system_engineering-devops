17dca81c535e40e0a8ec123337a4184d4e3f2e1c
Isek7 <bengabrielisek@gmail.com>
2024-03-05 16:15:30 +0100

    All fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,10 @@
+#!/usr/bin/python3
+"""
+Function that queries the Reddit API and returns
+the number of subscribers for a given subreddit.
+"""
 import requests
+import sys
 
 def number_of_subscribers(subreddit):
     """
