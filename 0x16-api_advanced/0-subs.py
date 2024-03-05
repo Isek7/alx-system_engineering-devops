f106bdfd70970ab9fc4c25f485bb88ce758d94fb
Isek7 <bengabrielisek@gmail.com>
2024-03-05 16:21:58 +0100

    Updated all

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,27 +6,22 @@ the number of subscribers for a given subreddit.
 import requests
 import sys
 
+
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
 
-    Args:
-    - subreddit (str): The name of the subreddit.
+    headers = {
+        'User-Agent': u_agent
+    }
 
-    Returns:
-    - int: The number of subscribers for the given subreddit. If the subreddit is invalid, returns 0.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            subscribers = data['data']['subscribers']
-            return subscribers
-        else:
-            return 0
-    except Exception as e:
-        print(f"Error: {e}")
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code != 200:
         return 0
+    dic = res.json()
+    if 'data' not in dic:
+        return 0
+    if 'subscribers' not in dic.get('data'):
+        return 0
+    return res.json()['data']['subscribers']
