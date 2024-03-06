cf5f84cdfab231901c709963daa83877900cd327
Isek7 <bengabrielisek@gmail.com>
2024-03-06 01:54:39 +0100

    Updating

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-'''Module containing subreddit api task'''
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    '''
-    queries the Reddit API and returns the number of
-    subscribers (not active users, total subscribers)
-    for a given subreddit.
-    If an invalid subreddit is given, the function should return 0.
-    '''
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-            "User-Agent": "linux:0x16.api.advanced:v1.0.0"
-            }
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-'''Module containing subreddit api task'''
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    '''
-    queries the Reddit API and prints the titles of the first 10 hot posts
-    listed for a given subreddit.
-    If an invalid subreddit is given, the function should print None.
-    '''
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-            "User-Agent": "linux:0x16.api.advanced:v1.0.0"
-            }
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
     params = {
         "limit": 10
     }
@@ -20,5 +16,6 @@ def top_ten(subreddit):
                             allow_redirects=False)
     if response.status_code == 404:
         print("None")
+        return
     results = response.json().get("data")
-    [print(item.get("data").get("title")) for item in results.get("children")]
+    [print(c.get("data").get("title")) for c in results.get("children")]
